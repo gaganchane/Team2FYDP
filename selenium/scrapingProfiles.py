06208155a8f6c07540d6b2e6df2a47444672678b
SunnyShikhar <shikhar.gaur.13@gmail.com>
2018-02-09 12:17:56 -0500

    added csv file and refactored code

--- selenium/scrapingProfiles.py
+++ selenium/scrapingProfiles.py
@@ -1,4 +1,5 @@
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -8,8 +9,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 
-# https://stackoverflow.com/questions/41502619/mouse-over-operation-in-python-selenium
-
 def startChrome():
     options = Options() 
     options.add_argument("user-data-dir=/Users/sunnygaur/Library/Application Support/Google/Chrome/")
@@ -17,9 +16,34 @@ def startChrome():
     driver.set_page_load_timeout(60)
     return driver
 
+def readCSVFile(directory):
+  graduates = pd.read_csv(directory)
+  return graduates
+
+def loadProfile(driver, URL, cssSelector):
+  print(URL)
+  driver.get(URL)
+  time.sleep(10)
+  element = driver.find_element_by_css_selector(cssSelector)
+  if element.is_displayed():
+    print ("Download button found")
+  else:
+    print ("Download button not found")
+  try:
+    element.click()
+    print("Button clicked, activating Almabase search")
+  except WebDriverException:
+    print ("Button is not clickable")
+  element.send_keys(Keys.RETURN)
+  time.sleep(17)
+
+graduates = readCSVFile('testFile.csv')
 driver = startChrome()
-linkedInURL = 'https://www.linkedin.com/'
-# driver.get(linkedInURL);
+downloadButton = "#ab-indeed-capture a"
+
+for row in graduates['URL']:
+  loadProfile(driver, row, downloadButton)
+
 
 # typeEmail = driver.find_element_by_css_selector(".login-email")
 # typePassword = driver.find_element_by_css_selector(".login-password")
@@ -27,78 +51,4 @@ linkedInURL = 'https://www.linkedin.com/'
 # typePassword.send_keys(TYPE PASSWORD)
 # time.sleep(5)
 # typePassword.submit()
-# time.sleep(5)
-
-driver.get('https://www.linkedin.com/in/faisale/');
-time.sleep(10)
-
-print("searching")
-element = driver.find_element_by_css_selector("#ab-indeed-capture a") 
-if element.is_displayed():
-  print ("Element found")
-else:
-  print ("Element not found")
-
-print(element.is_enabled())
-print(element.is_displayed())
-try:
-    element.click()
-    print("click was attempted")
-except WebDriverException:
-    print ("Element is not clickable")
-element.send_keys(Keys.RETURN)
-time.sleep(20)
-
-driver.get('https://www.linkedin.com/in/j2colaco/');
-time.sleep(10)
-
-print("searching")
-element = driver.find_element_by_css_selector("#ab-indeed-capture a") 
-if element.is_displayed():
-  print ("Element found")
-else:
-  print ("Element not found")
-
-print(element.is_enabled())
-print(element.is_displayed())
-try:
-    element.click()
-    print("click was attempted")
-except WebDriverException:
-    print ("Element is not clickable")
-element.send_k+eys(Keys.RETURN)
-time.sleep(20)
-
-# driver.execute_script()
-
-# print("starting")
-# elem = driver.find_element_by_css_selector(".pv-top-card-section__actions--at-bottom span a")
-# elem.click()
-# elem = driver.find_element_by_css_selector("#ab-indeed-capture a")
-
-# almabase = WebDriverWait(driver, 20).until(
-# EC.element_to_be_clickable((By.CSS_SELECTOR, "#ab-indeed-capture")));
-# almabase.click()
-# print("gotem?")
-# almabase = driver.find_element_by_css_selector("#ab-indeed-capture a")
-# almabase.click()
-
-# driver.find_element_by_css_selector("#ab-indeed-capture span").click()
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# time.sleep(5) # Let the user actually see something!
-# driver.quit()
-
-# driver.get('google.ca')
-# driver = webdriver.Chrome()
-# Sign into google to access chrome extensions?
-# driver.get('https://accounts.google.com/');
-# time.sleep(5) # Let the user actually see something!
-# gEmail = driver.find_element_by_css_selector("#identifierId")
-# gEmail.send_keys(TYPE EMAIL)
-# driver.find_element_by_css_selector("#identifierNext").click()
-# time.sleep(3)
-# gPassword = driver.find_element_by_name("password")
-# gPassword.send_keys(TYPE PASSWORD)
-# driver.find_element_by_css_selector("#passwordNext").click()
 # time.sleep(5)
