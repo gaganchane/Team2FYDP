62295371a5750c521c5b9bd49de36e1ed3f4230f
Gagan Chane <chanegs2000@gmail.com>
2018-02-21 01:01:27 -0500

    Refactored code - Functionized the function to format dates and extract date components

--- data_cleaning/alma_cleaning_script.R
+++ data_cleaning/alma_cleaning_script.R
@@ -15,7 +15,7 @@ clean <- read.csv("mgmt_data_clean.csv")
 View(clean)
 
 # read almabase output
-alma_data <- read.csv("data-buddy-4.csv", header=T, na.strings=c("", " ", "NA"), stringsAsFactors=FALSE)
+alma_data <- read.csv("data-buddy-5.csv", header=T, na.strings=c("", " ", "NA"), stringsAsFactors=FALSE)
 View(alma_data)
 
 # create vector of relevant columns
@@ -30,15 +30,71 @@ relevant_cols <- c("Name", "Linkedin.URL", "Education..School.1", "Education..Du
 # create a new data frame with only relevant columns
 relevant_data <- alma_data[relevant_cols]
 relevant_data <- data.frame(lapply(relevant_data, trimws), stringsAsFactors = FALSE)
-relevant_data[1,5]
-View(relevant_data)
 
 # initialize data frame to which final output will be added
 names <- c("ID", "WORK_ID", "COOP_ID", "Name", "URL", "Year", "Company", "Position", "Duration", "Start.Date", 
-           "Start.Month", "Start.Year", "End.Date.pres", "End.Year", "End.Month", "Full.Location", "City", "Country")
+           "Start.Month", "Start.Year", "End.Date.pres", "End.Month", "End.Year", "Full.Location", "City", "Country")
 init_df <- data.frame(matrix(ncol = 18, nrow = 0), stringsAsFactors=FALSE)
 colnames(init_df) <- names
 count <- 1
+
+date.format <- function(duration){
+  emp_duration_split <- strsplit(duration, " - ")
+  start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+  
+  if (!is.na(start_duration_split[[1]][2])){
+    start_month <- start_duration_split[[1]][1]
+    start_year <- start_duration_split[[1]][2]
+    
+    if(emp_duration_split[[1]][2] == "Present"){
+      
+      end_duration_split <- "pres"
+      end_month <- "pres"
+      end_year <- "pres"
+    } else {
+      end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
+      end_month <- end_duration_split[[1]][1]
+      end_year <- end_duration_split[[1]][2]
+    }
+    
+    if(match(start_month, month.abb) < 10){
+      start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
+    } else {
+      start_month_num <- match(start_month, month.abb)
+    }
+    
+    start_date <- paste(c(start_month_num, start_year), collapse="/") 
+    
+    if(end_month == "pres"){
+      end_month_num <- "pres"
+    } else if (match(end_month, month.abb) < 10){
+      end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
+    } else {
+      end_month_num <- match(end_month, month.abb)
+    }
+    
+    if(end_month_num == "pres"){
+      end_date <- end_month_num
+    } else {
+      end_date <- paste(c(end_month_num, end_year), collapse="/") 
+    }
+    
+    final_duration <- paste(c(start_date, end_date), collapse="-")
+    
+  } else {
+    
+    start_date <- emp_duration_split[[1]][1]
+    start_month_num <- "NA"
+    start_year <- emp_duration_split[[1]][1]
+    end_date <- emp_duration_split[[1]][2]
+    end_month_num <- "NA"
+    end_year <- emp_duration_split[[1]][2]
+    final_duration <- paste(c(start_date, end_date), collapse="-")
+  }
+  
+  return.vector <- c(start_date, start_month_num, start_year, end_date, end_month_num, end_year, final_duration)
+  return(return.vector)
+}
 
 # populate init_df with almabase output data
 for(i in 1:nrow(relevant_data)){
@@ -61,72 +117,20 @@ for(i in 1:nrow(relevant_data)){
     init_df[count, 7] <- relevant_data[i,7]
     init_df[count, 8] <- relevant_data[i,8]
     
-    emp_duration_split <- strsplit(relevant_data[i, 9], " - ")
-    start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+    date.vector <- date.format(relevant_data[i, 9])
     
-    if (!is.na(start_duration_split[[1]][2])){
-      start_month <- start_duration_split[[1]][1]
-      start_year <- start_duration_split[[1]][2]
-      
-      if(emp_duration_split[[1]][2] == "Present"){
-        
-        end_duration_split <- "pres"
-        end_month <- "pres"
-        end_year <- "pres"
-      } else {
-        end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
-        end_month <- end_duration_split[[1]][1]
-        end_year <- end_duration_split[[1]][2]
-      }
-      
-      if(match(start_month, month.abb) < 10){
-        start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
-      } else {
-        start_month_num <- match(start_month, month.abb)
-      }
-      
-      start_date <- paste(c(start_month_num, start_year), collapse="/") 
-      
-      if(end_month == "pres"){
-        end_month_num <- "pres"
-      } else if (match(end_month, month.abb) < 10){
-        end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
-      } else {
-        end_month_num <- match(end_month, month.abb)
-      }
-      
-      if(end_month_num == "pres"){
-        end_date <- end_month_num
-      } else {
-        end_date <- paste(c(end_month_num, end_year), collapse="/") 
-      }
-      
-      duration <- paste(c(start_date, end_date), collapse="-")
-      
-    } else {
-      
-      start_date <- emp_duration_split[[1]][1]
-      start_month_num <- "NA"
-      start_year <- emp_duration_split[[1]][1]
-      end_date <- emp_duration_split[[1]][2]
-      end_month_num <- "NA"
-      end_year <- emp_duration_split[[1]][2]
-      duration <- paste(c(start_date, end_date), collapse="-")
-    }
-    
-    init_df[count, 9] <- duration
-    init_df[count, 10] <- start_date
-    init_df[count, 11] <- start_month_num
-    init_df[count, 12] <- start_year
-    init_df[count, 13] <- end_date
-    init_df[count, 14] <- end_month_num
-    init_df[count, 15] <- end_year
+    init_df[count, 9] <- date.vector[7]
+    init_df[count, 10] <- date.vector[1]
+    init_df[count, 11] <- date.vector[2]
+    init_df[count, 12] <- date.vector[3]
+    init_df[count, 13] <- date.vector[4]
+    init_df[count, 14] <- date.vector[5]
+    init_df[count, 15] <- date.vector[6]
     init_df[count, 16] <- relevant_data[i,10]
     init_df[count, 17] <- NA
     init_df[count, 18] <- NA
     
     count = count + 1
-    print(count)
   }
   
   if(!is.na(relevant_data[i, 12])){
@@ -147,72 +151,20 @@ for(i in 1:nrow(relevant_data)){
     init_df[count, 7] <- relevant_data[i,11]
     init_df[count, 8] <- relevant_data[i,12]
     
-    emp_duration_split <- strsplit(relevant_data[i, 13], " - ")
-    start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+    date.vector <- date.format(relevant_data[i, 13])
     
-    if (!is.na(start_duration_split[[1]][2])){
-      start_month <- start_duration_split[[1]][1]
-      start_year <- start_duration_split[[1]][2]
-      
-      if(emp_duration_split[[1]][2] == "Present"){
-        
-        end_duration_split <- "pres"
-        end_month <- "pres"
-        end_year <- "pres"
-      } else {
-        end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
-        end_month <- end_duration_split[[1]][1]
-        end_year <- end_duration_split[[1]][2]
-      }
-      
-      if(match(start_month, month.abb) < 10){
-        start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
-      } else {
-        start_month_num <- match(start_month, month.abb)
-      }
-      
-      start_date <- paste(c(start_month_num, start_year), collapse="/") 
-      
-      if(end_month == "pres"){
-        end_month_num <- "pres"
-      } else if (match(end_month, month.abb) < 10){
-        end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
-      } else {
-        end_month_num <- match(end_month, month.abb)
-      }
-      
-      if(end_month_num == "pres"){
-        end_date <- end_month_num
-      } else {
-        end_date <- paste(c(end_month_num, end_year), collapse="/") 
-      }
-      
-      duration <- paste(c(start_date, end_date), collapse="-")
-      
-    } else {
-      
-      start_date <- emp_duration_split[[1]][1]
-      start_month_num <- "NA"
-      start_year <- emp_duration_split[[1]][1]
-      end_date <- emp_duration_split[[1]][2]
-      end_month_num <- "NA"
-      end_year <- emp_duration_split[[1]][2]
-      duration <- paste(c(start_date, end_date), collapse="-")
-    }
-    
-    init_df[count, 9] <- duration
-    init_df[count, 10] <- start_date
-    init_df[count, 11] <- start_month_num
-    init_df[count, 12] <- start_year
-    init_df[count, 13] <- end_date
-    init_df[count, 14] <- end_month_num
-    init_df[count, 15] <- end_year
+    init_df[count, 9] <- date.vector[7]
+    init_df[count, 10] <- date.vector[1]
+    init_df[count, 11] <- date.vector[2]
+    init_df[count, 12] <- date.vector[3]
+    init_df[count, 13] <- date.vector[4]
+    init_df[count, 14] <- date.vector[5]
+    init_df[count, 15] <- date.vector[6]
     init_df[count, 16] <- relevant_data[i,14]
     init_df[count, 17] <- NA
     init_df[count, 18] <- NA
     
     count = count + 1
-    print(count)
   }
   
   if(!is.na(relevant_data[i, 16])){
@@ -233,72 +185,20 @@ for(i in 1:nrow(relevant_data)){
     init_df[count, 7] <- relevant_data[i,15]
     init_df[count, 8] <- relevant_data[i,16]
     
-    emp_duration_split <- strsplit(relevant_data[i, 17], " - ")
-    start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+    date.vector <- date.format(relevant_data[i, 17])
     
-    if (!is.na(start_duration_split[[1]][2])){
-      start_month <- start_duration_split[[1]][1]
-      start_year <- start_duration_split[[1]][2]
-      
-      if(emp_duration_split[[1]][2] == "Present"){
-        
-        end_duration_split <- "pres"
-        end_month <- "pres"
-        end_year <- "pres"
-      } else {
-        end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
-        end_month <- end_duration_split[[1]][1]
-        end_year <- end_duration_split[[1]][2]
-      }
-      
-      if(match(start_month, month.abb) < 10){
-        start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
-      } else {
-        start_month_num <- match(start_month, month.abb)
-      }
-      
-      start_date <- paste(c(start_month_num, start_year), collapse="/") 
-      
-      if(end_month == "pres"){
-        end_month_num <- "pres"
-      } else if (match(end_month, month.abb) < 10){
-        end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
-      } else {
-        end_month_num <- match(end_month, month.abb)
-      }
-      
-      if(end_month_num == "pres"){
-        end_date <- end_month_num
-      } else {
-        end_date <- paste(c(end_month_num, end_year), collapse="/") 
-      }
-      
-      duration <- paste(c(start_date, end_date), collapse="-")
-      
-    } else {
-      
-      start_date <- emp_duration_split[[1]][1]
-      start_month_num <- "NA"
-      start_year <- emp_duration_split[[1]][1]
-      end_date <- emp_duration_split[[1]][2]
-      end_month_num <- "NA"
-      end_year <- emp_duration_split[[1]][2]
-      duration <- paste(c(start_date, end_date), collapse="-")
-    }
-    
-    init_df[count, 9] <- duration
-    init_df[count, 10] <- start_date
-    init_df[count, 11] <- start_month_num
-    init_df[count, 12] <- start_year
-    init_df[count, 13] <- end_date
-    init_df[count, 14] <- end_month_num
-    init_df[count, 15] <- end_year
+    init_df[count, 9] <- date.vector[7]
+    init_df[count, 10] <- date.vector[1]
+    init_df[count, 11] <- date.vector[2]
+    init_df[count, 12] <- date.vector[3]
+    init_df[count, 13] <- date.vector[4]
+    init_df[count, 14] <- date.vector[5]
+    init_df[count, 15] <- date.vector[6]
     init_df[count, 16] <- relevant_data[i,18]
     init_df[count, 17] <- NA
     init_df[count, 18] <- NA
     
     count = count + 1
-    print(count)
   }
   
   if(!is.na(relevant_data[i, 20])){
@@ -319,72 +219,20 @@ for(i in 1:nrow(relevant_data)){
     init_df[count, 7] <- relevant_data[i,19]
     init_df[count, 8] <- relevant_data[i,20]
     
-    emp_duration_split <- strsplit(relevant_data[i, 21], " - ")
-    start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+    date.vector <- date.format(relevant_data[i, 21])
     
-    if (!is.na(start_duration_split[[1]][2])){
-      start_month <- start_duration_split[[1]][1]
-      start_year <- start_duration_split[[1]][2]
-      
-      if(emp_duration_split[[1]][2] == "Present"){
-        
-        end_duration_split <- "pres"
-        end_month <- "pres"
-        end_year <- "pres"
-      } else {
-        end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
-        end_month <- end_duration_split[[1]][1]
-        end_year <- end_duration_split[[1]][2]
-      }
-      
-      if(match(start_month, month.abb) < 10){
-        start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
-      } else {
-        start_month_num <- match(start_month, month.abb)
-      }
-      
-      start_date <- paste(c(start_month_num, start_year), collapse="/") 
-      
-      if(end_month == "pres"){
-        end_month_num <- "pres"
-      } else if (match(end_month, month.abb) < 10){
-        end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
-      } else {
-        end_month_num <- match(end_month, month.abb)
-      }
-      
-      if(end_month_num == "pres"){
-        end_date <- end_month_num
-      } else {
-        end_date <- paste(c(end_month_num, end_year), collapse="/") 
-      }
-      
-      duration <- paste(c(start_date, end_date), collapse="-")
-      
-    } else {
-      
-      start_date <- emp_duration_split[[1]][1]
-      start_month_num <- "NA"
-      start_year <- emp_duration_split[[1]][1]
-      end_date <- emp_duration_split[[1]][2]
-      end_month_num <- "NA"
-      end_year <- emp_duration_split[[1]][2]
-      duration <- paste(c(start_date, end_date), collapse="-")
-    }
-    
-    init_df[count, 9] <- duration
-    init_df[count, 10] <- start_date
-    init_df[count, 11] <- start_month_num
-    init_df[count, 12] <- start_year
-    init_df[count, 13] <- end_date
-    init_df[count, 14] <- end_month_num
-    init_df[count, 15] <- end_year
+    init_df[count, 9] <- date.vector[7]
+    init_df[count, 10] <- date.vector[1]
+    init_df[count, 11] <- date.vector[2]
+    init_df[count, 12] <- date.vector[3]
+    init_df[count, 13] <- date.vector[4]
+    init_df[count, 14] <- date.vector[5]
+    init_df[count, 15] <- date.vector[6]
     init_df[count, 16] <- relevant_data[i,22]
     init_df[count, 17] <- NA
     init_df[count, 18] <- NA
     
     count = count + 1
-    print(count)
   }
   
   if(!is.na(relevant_data[i, 24])){
@@ -405,72 +253,20 @@ for(i in 1:nrow(relevant_data)){
     init_df[count, 7] <- relevant_data[i,23]
     init_df[count, 8] <- relevant_data[i,24]
     
-    emp_duration_split <- strsplit(relevant_data[i, 25], " - ")
-    start_duration_split <- strsplit(emp_duration_split[[1]][1], " ")
+    date.vector <- date.format(relevant_data[i, 25])
     
-    if (!is.na(start_duration_split[[1]][2])){
-      start_month <- start_duration_split[[1]][1]
-      start_year <- start_duration_split[[1]][2]
-      
-      if(emp_duration_split[[1]][2] == "Present"){
-        
-        end_duration_split <- "pres"
-        end_month <- "pres"
-        end_year <- "pres"
-      } else {
-        end_duration_split <- strsplit(emp_duration_split[[1]][2], " ")
-        end_month <- end_duration_split[[1]][1]
-        end_year <- end_duration_split[[1]][2]
-      }
-      
-      if(match(start_month, month.abb) < 10){
-        start_month_num <- paste(c("0", match(start_month, month.abb)), collapse="") 
-      } else {
-        start_month_num <- match(start_month, month.abb)
-      }
-      
-      start_date <- paste(c(start_month_num, start_year), collapse="/") 
-      
-      if(end_month == "pres"){
-        end_month_num <- "pres"
-      } else if (match(end_month, month.abb) < 10){
-        end_month_num <- paste(c("0", match(end_month, month.abb)), collapse="")
-      } else {
-        end_month_num <- match(end_month, month.abb)
-      }
-      
-      if(end_month_num == "pres"){
-        end_date <- end_month_num
-      } else {
-        end_date <- paste(c(end_month_num, end_year), collapse="/") 
-      }
-      
-      duration <- paste(c(start_date, end_date), collapse="-")
-      
-    } else {
-      
-      start_date <- emp_duration_split[[1]][1]
-      start_month_num <- "NA"
-      start_year <- emp_duration_split[[1]][1]
-      end_date <- emp_duration_split[[1]][2]
-      end_month_num <- "NA"
-      end_year <- emp_duration_split[[1]][2]
-      duration <- paste(c(start_date, end_date), collapse="-")
-    }
-    
-    init_df[count, 9] <- duration
-    init_df[count, 10] <- start_date
-    init_df[count, 11] <- start_month_num
-    init_df[count, 12] <- start_year
-    init_df[count, 13] <- end_date
-    init_df[count, 14] <- end_month_num
-    init_df[count, 15] <- end_year
+    init_df[count, 9] <- date.vector[7]
+    init_df[count, 10] <- date.vector[1]
+    init_df[count, 11] <- date.vector[2]
+    init_df[count, 12] <- date.vector[3]
+    init_df[count, 13] <- date.vector[4]
+    init_df[count, 14] <- date.vector[5]
+    init_df[count, 15] <- date.vector[6]
     init_df[count, 16] <- relevant_data[i,26]
     init_df[count, 17] <- NA
     init_df[count, 18] <- NA
     
     count = count + 1
-    print(count)
   }
 }
 
@@ -488,7 +284,7 @@ init_df <- arrange(init_df, ID, Start.Year, Start.Month)
 coopcount = 1
 workcount = 1
 
-if(init_df[1, 'Start.Year'] < init_df[1, 'Year'] & (init_df[i, 'End.Date.pres'] != "pres")){
+if(init_df[1, 'Start.Year'] < init_df[1, 'Year'] & (init_df[1, 'End.Date.pres'] != "pres")){
   init_df[1, 'COOP_ID'] <- coopcount
   init_df[1, 'WORK_ID'] <- NA
   coopcount = coopcount + 1
@@ -497,7 +293,7 @@ if(init_df[1, 'Start.Year'] < init_df[1, 'Year'] & (init_df[i, 'End.Date.pres'] 
   init_df[1, 'COOP_ID'] <- NA
   workcount = workcount + 1
 }
-  
+
 for (i in 2:nrow(init_df)){
   
   if(init_df[i, 'ID'] != init_df[i-1, 'ID']){
@@ -515,9 +311,6 @@ for (i in 2:nrow(init_df)){
     workcount = workcount + 1
   }
 }
-
-# write init_df to csv
-write.csv(init_df, file = "output.csv", row.names = FALSE)
 
 # determine location (city and country) and add to init_df
 
@@ -544,25 +337,23 @@ for (i in 1:nrow(init_df)){
   }
   
   location.split <- unlist(strsplit(init_df[i, "Full.Location"], split=" "))
-
+  
   if(tail(location.split, n=1) == "Canada"){
     init_df[i,"Country"] <- "Canada"
     location.split <- head(location.split, -1)
     location.split.concat <- do.call(c, lapply(seq_along(location.split), function(i) combn(location.split, i, FUN = list)))
-
+    
     for(j in 1:length(location.split.concat)){
       location.split.concat[[j]] <- paste(location.split.concat[[j]], collapse = ' ')
     }
     unlist(location.split.concat)
-
+    
     for(k in 1:length(location.split.concat)){
-     # if(k == 1){
-        for(l in 1:nrow(canada.cities)){
-          if(tolower(location.split.concat[k]) == tolower(canada.cities[l,1])){
-            init_df[i, "City"] <- location.split.concat[k]
-          }
+      for(l in 1:nrow(canada.cities)){
+        if(tolower(location.split.concat[k]) == tolower(canada.cities[l,1])){
+          init_df[i, "City"] <- location.split.concat[k]
         }
-      #}
+      }
     }
     next
   }
@@ -610,3 +401,6 @@ for (i in 1:nrow(init_df)){
   }
 }
 View(init_df)
+
+# write init_df to csv
+write.csv(init_df, file = "output.csv", row.names = FALSE)
